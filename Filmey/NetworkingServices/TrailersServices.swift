1282fd8598abd8db3432896df4174f05302a8ca7
Asmaa <asmaatarek93@gmail.com>
2020-01-09 02:56:35 +0200

    c

--- Filmey/NetworkingServices/TrailersServices.swift
+++ Filmey/NetworkingServices/TrailersServices.swift
@@ -11,32 +11,32 @@ class TrailerServices
     
 {
     var trailers = [TrailerData]()
-     func getTrailers(movieID: Int,completion: @escaping (_ jsonData: [TrailerData],_ error:Error?) -> Void) {
-          let url = URL(string: "https://api.themoviedb.org/3/movie/\(movieID)/videos?api_key=\(apiKey)&language=en-US")
-          let request = URLRequest(url: url!)
-          let session = URLSession(configuration: URLSessionConfiguration.default)
-          let task = session.dataTask(with: request) { (data, respose, error) in
-              do
-              {
-                  let json  = try JSONSerialization.jsonObject(with: data!, options: .allowFragments) as! [String:Any]
-                  let result = json["results"] as! [[String:Any]]
-                  for i in 0..<result.count
-                  {
-                      let dic = result[i]
-                      self.trailers.append(TrailerData(name: dic["name"] as! String, key: dic["key"] as! String, site: dic["site"] as! String, type: dic["type"] as! String))
-                  }
-                  
-                  completion(self.trailers,nil)
-                  
-              }
-                  
-              catch
-              {
-                  print("error")
-              }
-          }
-          
-          
-          task.resume()
-      }
+    func getTrailers(movieID: Int,completion: @escaping (_ jsonData: [TrailerData],_ error:Error?) -> Void) {
+        let url = URL(string: "https://api.themoviedb.org/3/movie/\(movieID)/videos?api_key=\(apiKey)&language=en-US")
+        let request = URLRequest(url: url!)
+        let session = URLSession(configuration: URLSessionConfiguration.default)
+        let task = session.dataTask(with: request) { (data, respose, error) in
+            do
+            {
+                guard let json  = try JSONSerialization.jsonObject(with: data!, options: .allowFragments) as? [String:Any] else {return}
+                guard let result = json["results"] as? [[String:Any]] else {return}
+                for i in 0..<result.count
+                {
+                    let dic = result[i]
+                    self.trailers.append(TrailerData(name: dic["name"] as? String ?? "", key: dic["key"] as? String ?? "", site: dic["site"] as? String ?? "", type: dic["type"] as? String ?? ""))
+                }
+                
+                completion(self.trailers,nil)
+                
+            }
+                
+            catch
+            {
+                print("error")
+            }
+        }
+        
+        
+        task.resume()
+    }
 }
